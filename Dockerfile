24bfb2c079094979da2b427d58d06b4ccfa478c7
Camila Jara Fuenzalida <cami@MacBook-Pro-de-Camila.local>
2026-06-27 16:38:18 -0400

    first API

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
+# 1. environment with python pre-installed
 FROM python:3.12-slim
 
 WORKDIR /app
 
-COPY . .
+# 2. install
+# fastapi: library for create API of easy form
+# uvicorn: Server that expose our API created with fastapi
+RUN pip install fastapi uvicorn
 
-CMD ["python3", "main.py"]
+# 3. Copi main file to docker
+COPY main.py .
+
+# 4. power on to Uvicorn
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello from Railway!")
+from fastapi import FastAPI
+
+# 1. Initialize the application (The brain communicating with Uvicorn)
+app = FastAPI()
+
+# 2. Create a basic route (The Hello World)
+# When Uvicorn receives a visitor at the main door ("/"), it triggers this.
+@app.get("/")
+def read_root():
+    return {"message": "Hello World! This is my first cloud container 🚀"}
+
+# 3. Create a route returning static data
+# When Uvicorn receives a visitor asking for "/data", it returns this JSON.
+@app.get("/data")
+def get_data():
+    return {
+        "user": "Analyst_01",
+        "role": "Data Scientist",
+        "status": "Learning Docker"
+    }
